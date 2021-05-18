f409c273d05086b289f9d41519fc1a561096953c
sehoon <gsh723@naver.com>
2021-05-18 21:23:35 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode> // 'use strict'와 같은 역할
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
