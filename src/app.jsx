66e47057fa5ebea518aca5ed4c68d5f022978d73
sehoon <gsh723@naver.com>
2021-05-21 00:45:37 +0900

    1. 검색기능 리팩토링 및 environment variable 설정 및 gitignore 이용

--- src/app.jsx
+++ src/app.jsx
@@ -3,38 +3,20 @@ import styles from "./app.module.css";
 import Lists from './components/lists/lists';
 import Input from './components/search/input';
 
-function App(){
+function App({youtube}){
   // [state sector]
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]); 
 
   // [METHOD]
   // 1) Get API Method
-  useEffect(()=>{    
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=AIzaSyDGgWM2b6ovCQZJ77_AlhP5MKVWKr2gGEw", requestOptions)
-    .then(response => response.json()) // text() => json()로 바꿔줬다.
-    .then(result =>setVideos(result.items))
-    .catch(error => console.log('error', error));
-  },[]);  
+  useEffect(()=>youtube
+                  .mostPopular()
+                  .then(videos => setVideos(videos)),[]);  
   
   // 2) after searching, get API
-  const onSearch = useCallback(input=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyDGgWM2b6ovCQZJ77_AlhP5MKVWKr2gGEw&part=snippet&maxResults=25&q=${input}&type=video&key=AIzaSyDGgWM2b6ovCQZJ77_AlhP5MKVWKr2gGEw`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map((item)=>({...item, id:item.id.videoId}))) //key가 two childern뜨는 문제 해결하기 위해, id값만 따로 설정해준다.
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
-  },[videos])
-  
+  const onSearch = input=>youtube
+                            .search(input)
+                            .then(videos => setVideos(videos));  
   // [return sector]
   return (
     <div className={styles.app}>
